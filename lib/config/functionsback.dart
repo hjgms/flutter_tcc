3bb51759784b27788c3c74c65011f5e8598bb659
Rafael Ribeiro dos Santos <72527726+hjgms@users.noreply.github.com>
2023-03-13 20:25:36 -0300

    Update functionsback.dart

--- lib/config/functionsback.dart
+++ lib/config/functionsback.dart
@@ -1,70 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-usuariosGet(TextEditingController email,TextEditingController password, errorTextEmail, errorTextPassword) async{
-  CollectionReference users = FirebaseFirestore.instance.collection('usuarios');
-  var limit = 30;
-  var credentials = {
-    "email": email.text,
-    "password": password.text,
-    "pass": false
-  };
-  for (var i = 0; i < limit; i++) {
-    await users.doc("$i").get().then( (value){ 
-      if(value["credentials"]["email"] == credentials["email"]){
-        if(value["credentials"]["password"] == credentials["password"]){
-          credentials["pass"] = true;
-        }else{
-          errorTextPassword = "Password error";
-        }
-      }else{
-        errorTextEmail = "Email error";
-      }
-    }); 
-    if(credentials["pass"] == true){
-      return true;
-    }
+//create acout for new user
+createAcount(emailAddress,password) async{
+  try {
+    final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: emailAddress,
+      password: password,
+    );
+  } on FirebaseAuthException catch (e) {
+    print("error");
+  } catch (e) {
+    print("error");
   }
-  return false;
 }
-
-//para testes ainda ....
-//get - global
-getdb(String? collection,String id) async{
-	var ID = "";
-	var from;
-	try{
-		ID = Number(id);
-	}catch{}
-	if(ID == ""){
-		from = FirebaseFirestore.instance.collection("${collection}").doc();	
-	}else{
-		from = FirebaseFirestore.instance.collection("${collection}").doc("${ID}");
-	}
-	await from.get().then((data){
-		return data.json();
-	}).catch((err){
-		print(err);
-		return false;
-	});
+//test for login user
+loginAcount(TextEditingController emailAddress,TextEditingController password) async{
+  var result;
+  try {
+    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailAddress.text,
+      password: password.text
+    );
+    global.authentication = true;
+    global.credentialUser = {
+      "login": true,
+      "user": credential.user,
+      "info": credential.additionalUserInfo
+    };
+    result = "sucess";
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'user-not-found') {
+      print("error");
+    } else if (e.code == 'wrong-password') {
+      print("error");
+    }
+    result = e.code;
+  }
+  return result;
 }
-//post - global
-postdb(String? collection,String id,Map obj){
-	var ID = "";
-	var from;
-	try{
-		ID = Number(id);
-	}catch{}
-	if(ID == ""){
-		from = FirebaseFirestore.instance.collection("${collection}").doc();	
-	}else{
-		from = FirebaseFirestore.instance.collection("${collection}").doc("${ID}");
-	}
-	from.add(obj).then((){
-		return true;
-	}).catch((err){
-		print(err);
-		return false;
-	});
+//sign out user
+signoutAcount() async{
+  await FirebaseAuth.instance.signOut();
 }
