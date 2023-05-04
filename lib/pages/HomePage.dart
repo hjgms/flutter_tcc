5702484132f9d713a3d10049a1d41ff46dbcc008
Rafael <72527726+hjgms@users.noreply.github.com>
2023-05-04 17:43:48 -0300

    04/05/2023

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -7,7 +7,7 @@ import 'package:flutter_application_firebase/pages/MessagePage.dart';
 import 'package:flutter_application_firebase/components/postHomeWidget.dart';
 
 //configs
-import 'package:flutter_application_firebase/config/globalvariables.dart' as global;
+import 'package:flutter_application_firebase/config/globals.dart' as global;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
