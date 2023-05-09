991db68fc91163be09af2fd934dd37f21e7d5fae
hjgms <rafaelesantos1101@gmail.com>
2023-05-09 16:57:02 -0300

    09/05/2023

--- lib/data/cache/cache.dart
+++ lib/data/cache/cache.dart
@@ -3,9 +3,6 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
-//models
-import 'package:flutter_application_firebase/data/cache/models/user.dart' as user;
-
 //instance storage
 Future<SharedPreferences> init() async {
   return await SharedPreferences.getInstance();
@@ -13,17 +10,15 @@ Future<SharedPreferences> init() async {
 
 Future getCacheUserUid() async {
   final storage = await init();
-  var memory = storage.getString("uid");
-  if(memory == null){
-    memory = "";
-  }
+  String? memory = storage.getString("uid");
+  memory ??= "";
   return memory;
 }
 
 Future getCacheUserAuth() async {
   final storage = await init();
   var memory = storage.getBool("auth");
-  if(memory == null){
+  if (memory == null) {
     storage.setBool("auth", false);
     memory = false;
   }
@@ -32,6 +27,26 @@ Future getCacheUserAuth() async {
 
 Future setCacheUserAuth(bool auth, String uid) async{
   final storage = await init();
-  storage.setBool("auth", auth);
-  storage.setString("uid", uid);
+  bool? status;
+  status = await storage.setBool("auth", auth);
+  if (status) {
+    status = null;
+    status = await storage.setString("uid", uid);
+    return status ? true : false;
+  }
+  return false;
+}
+
+Future<bool> setCacheUserData(String uid, Map data) async {
+  final storage = await init();
+  final valor = json.encode(data).toString();
+  bool? status = await storage.setString("userData", valor);
+  return status ? true : false;
+}
+
+Future getCacheUserData() async {
+  final storage = await init();
+  String? value = storage.getString("userData");
+  final resp = json.decode(value!);
+  return resp;
 }
